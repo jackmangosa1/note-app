77e9e576662822f9ca3252bdc8fa85754610cf18
Jack Mangosa <109428669+jackmangosa1@users.noreply.github.com>
2022-08-19 15:15:48 +0200

    Create the sidebar main

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import Sidebar from "./components/Sidebar";
 function App() {
   return (
     <div className="App">
-      <h1>Notes App</h1>
       <Sidebar/>
       <Main/>
     </div>
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -5,6 +5,17 @@ return(
             <h1>Notes</h1>
             <button>Add</button>
         </div>
+
+        <div className="app-sidebar-notes">
+            <div className="app-sidebar-note">
+                <div className="sidebar-note-title">
+                    <strong>Title</strong>
+                    <button>Delete</button>
+                </div>
+                <p>Note preview</p>
+                <small className="note-meta">Last modified [date]</small>
+            </div>
+        </div>
     </div>
 )
 }
