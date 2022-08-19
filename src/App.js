c4f1441793057cab075037a03a476e272de16f48
Jack Mangosa <109428669+jackmangosa1@users.noreply.github.com>
2022-08-19 17:28:08 +0200

    Create the note object
    Create the note object  and update sidebar.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import { useState } from "react";
+import uuid from "react-uuid";
 import Main from "./components/Main";
 import Sidebar from "./components/Sidebar";
 
 function App() {
   const[notesArray, setNotesArray] = useState([])
+
   const addNote = () => {
-    console.log("Item added")
+    const newNote = {
+      id: uuid(),
+      title: "Untitled note",
+      body: "",
+      lastModified: Date.now()
+    }
+
+    setNotesArray([newNote, ...notesArray])
   }
 
   return (
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,5 +1,6 @@
 function Sidebar({notes, addNote}){
-return(
+    return(
+
     <div className="app-sidebar">
         <div className="app-sidebar-header">
             <h1>Notes</h1>
@@ -11,11 +12,18 @@ return(
 
         <div className="app-sidebar-note">
             <div className="sidebar-note-title">
-                <strong>Title</strong>
+                <strong>{note.title}</strong>
                 <button>Delete</button>
             </div>
-            <p>Note preview</p>
-            <small className="note-meta">Last modified [date]</small>
+            <p>{note.body && note.body.substr(0, 100) + "..."}</p>
+            <small className="note-meta">
+                Last modified {new Date(note.lastModified).toLocaleDateString("en-GB", {
+                    hour: "2-digit",
+                    minute: "2-digit"
+
+                })}
+
+            </small>
         </div>
        ))}
              
