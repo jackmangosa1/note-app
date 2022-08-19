9462868be4956fe13d52a12d8936443867cfd363
Jack Mangosa <109428669+jackmangosa1@users.noreply.github.com>
2022-08-19 15:08:02 +0200

    Create side bar header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
-
+import Main from "./components/Main";
+import Sidebar from "./components/Sidebar";
 
 function App() {
   return (
     <div className="App">
       <h1>Notes App</h1>
+      <Sidebar/>
+      <Main/>
     </div>
   );
 }
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,5 +1,12 @@
 function Sidebar(){
-
+return(
+    <div className="app-sidebar">
+        <div className="app-sidebar-header">
+            <h1>Notes</h1>
+            <button>Add</button>
+        </div>
+    </div>
+)
 }
 
 export default Sidebar
