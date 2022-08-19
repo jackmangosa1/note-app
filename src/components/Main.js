038c6ab4c9c93a9b062c4eeef97b7259730ea91b
Jack Mangosa <109428669+jackmangosa1@users.noreply.github.com>
2022-08-19 15:39:31 +0200

    Update Main.js

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,5 +1,18 @@
 function Main(){
+return(
+    <div className="app-main">
+        <div className="app-main-note-edit">
 
+            <input type="text" id="title" autoFocus/>
+            <textarea id="body" placeholder="Write your text here..."></textarea>
+        </div>
+        <div className="app-main-note-preview">
+            <h1 className="preview-title">TITLE</h1>
+            <div className="markdown-preview">note preview</div>
+
+        </div>
+    </div>
+)
 }
 
 export default Main
