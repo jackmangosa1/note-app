c65576e614e6d7b25fb093d1352005ae469e3405
Jack Mangosa <109428669+jackmangosa1@users.noreply.github.com>
2022-08-19 16:32:42 +0200

    Crete notes state array and props in sidebar.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
+import { useState } from "react";
 import Main from "./components/Main";
 import Sidebar from "./components/Sidebar";
 
 function App() {
+  const[notesArray, setNotesArray] = useState([])
+  const addNote = () => {
+    console.log("Item added")
+  }
+
   return (
     <div className="App">
-      <Sidebar/>
+      <Sidebar
+      notes= {notesArray}
+      addNote={addNote}
+      />
       <Main/>
     </div>
   );
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,20 +1,26 @@
-function Sidebar(){
+function Sidebar({notes, addNote}){
 return(
     <div className="app-sidebar">
         <div className="app-sidebar-header">
             <h1>Notes</h1>
-            <button>Add</button>
+            <button onClick={addNote}>Add</button>
         </div>
 
         <div className="app-sidebar-notes">
-            <div className="app-sidebar-note">
-                <div className="sidebar-note-title">
-                    <strong>Title</strong>
-                    <button>Delete</button>
-                </div>
-                <p>Note preview</p>
-                <small className="note-meta">Last modified [date]</small>
+       {notes.map(note => (
+
+        <div className="app-sidebar-note">
+            <div className="sidebar-note-title">
+                <strong>Title</strong>
+                <button>Delete</button>
             </div>
+            <p>Note preview</p>
+            <small className="note-meta">Last modified [date]</small>
+        </div>
+       ))}
+             
+              
+             
         </div>
     </div>
 )
